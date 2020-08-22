de98ebd687faff60a2947d1766dc1be394afbc9a
janvandenpoel <jan.vandenpoel@vdpc.be>
2020-08-22 17:11:47 +0200

    Chapter 05

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:friend_favor/favor.dart';
+import 'package:friend_favor/friend.dart';
+import 'package:friend_favor/mock_values.dart';
+import 'package:flutter/services.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -26,92 +34,326 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Friend Favor Home Page'),
+      home: FavorsPage(
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FavorsPage extends StatefulWidget {
+  FavorsPage({
+    Key key,
+  }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => FavorsPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class FavorsPageState extends State<FavorsPage> {
+  // using mock values from mock_favors dart file for now
+  List<Favor> pendingAnswerFavors;
+  List<Favor> acceptedFavors;
+  List<Favor> completedFavors;
+  List<Favor> refusedFavors;
 
-  void _incrementCounter() {
+  @override  void initState() {
+    super.initState();
+    pendingAnswerFavors = List();
+    acceptedFavors = List();
+    completedFavors = List();
+    refusedFavors = List();
+    loadFavors();
+  }
+
+  void loadFavors() {
+    pendingAnswerFavors.addAll(mockPendingFavors);
+    acceptedFavors.addAll(mockDoingFavors);
+    completedFavors.addAll(mockCompletedFavors);
+    refusedFavors.addAll(mockRefusedFavors);
+  }
+
+  // part of FavorsPageState class
+  static FavorsPageState of(BuildContext context) {
+    return context.findAncestorStateOfType<FavorsPageState>();
+  }
+
+  void refuseToDo(Favor favor) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      pendingAnswerFavors.remove(favor);
+      refusedFavors.add(favor.copyWith(accepted: false));
     });
+  }
+
+  void acceptToDo(Favor favor) {
+      setState(() {
+        pendingAnswerFavors.remove(favor);
+
+        acceptedFavors.add(favor.copyWith(accepted: true));
+      });
+  }
+
+  Widget _buildCategoryTab(String title) {
+      return Tab(
+        child: Text(title),
+      );
+    }
+
+    Row _itemHeader(Favor favor) {
+      return Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              favor.friend.photoURL,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text("${favor.friend.name} asked you to...")
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget _itemFooter(Favor favor) {
+      if (favor.isCompleted) {
+        final format = DateFormat();
+        return Container(
+          margin: EdgeInsets.only(top: 8.0),
+          alignment: Alignment.centerRight,
+          child: Chip(
+            label: Text("Completed at: ${format.format(favor.completed)}"),
+          ),
+        );
+      }
+      if (favor.isRequested) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              child: Text("Refuse"),
+              onPressed: () {
+                FavorsPageState.of(context).refuseToDo(favor);
+                // we have changed _itemFooter to get the context so we
+                // can use it to fetch the favors page state
+              },
+            ),
+            FlatButton(
+              child: Text("Do"),
+              onPressed: () {
+                FavorsPageState.of(context).acceptToDo(favor);
+              },
+            )
+          ],
+        );
+      }
+      if (favor.isDoing) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              child: Text("give up"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text("complete"),
+              onPressed: () {},
+            )
+          ],
+        );
+      }
+      return Container();
+    }
+
+    Widget _favorsList(String title, List<Favor> favors) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Padding(
+            child: Text(title),
+            padding: EdgeInsets.only(top: 16.0),
+          ),
+          Expanded(
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: favors.length,
+              itemBuilder: (BuildContext context, int index) {
+                final favor = favors[index];
+                return Card(
+                  key: ValueKey(favor.uuid),
+                  margin: EdgeInsets.symmetric(vertical: 10.0,
+                      horizontal: 25.0),
+                  child: Padding(
+                    child: Column(
+                      children: <Widget>[
+                        _itemHeader(favor),
+                        Text(favor.description),
+                        _itemFooter(favor)
+                      ],
+                    ),
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      );
+    }
+
+    @override Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Your favors"),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              _buildCategoryTab("Requests"),
+              _buildCategoryTab("Doing"),
+              _buildCategoryTab("Completed"),
+              _buildCategoryTab("Refused"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            _favorsList("Pending Requests", pendingAnswerFavors),
+            _favorsList("Doing", acceptedFavors),
+            _favorsList("Completed", completedFavors),
+            _favorsList("Refused", refusedFavors),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => RequestFavorPage(
+                  friends: mockFriends,
+                ),
+              ),
+            );
+          },
+          tooltip: 'Ask a favor',
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+
+class RequestFavorPage extends StatefulWidget {
+  List<Friend> friends;
+
+  RequestFavorPage({Key key, this.friends}) : super(key: key);
+
+  @override
+  RequestFavorPageState createState() {
+    return new RequestFavorPageState();
+  }
+}
+
+class RequestFavorPageState extends State<RequestFavorPage> {
+  final _formKey = GlobalKey<FormState>();
+  Friend _selectedFriend = null;
+
+  static RequestFavorPageState of(BuildContext context) {
+    return context.findAncestorStateOfType<RequestFavorPageState>();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Requesting a favor"),
+        leading: CloseButton(),
+        actions: <Widget>[
+          Builder(
+            builder: (context) => FlatButton(
+              child: Text("SAVE"),
+              textColor: Colors.white,
+              onPressed: () {
+                RequestFavorPageState.of(context).save();
+              },
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              DropdownButtonFormField<Friend>(
+                value: _selectedFriend,
+                onChanged: (friend) {
+                  setState(() {
+                    _selectedFriend = friend;
+                  });
+                },
+                items: widget.friends
+                    .map(
+                      (f) => DropdownMenuItem<Friend>(
+                    value: f,
+                    child: Text(f.name),
+                  ),
+                )
+                    .toList(),
+                validator: (friend) {
+                  if (friend == null) {
+                    return "You must select a friend to ask the favor";
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                height: 16.0,
+              ),
+              Text("Favor description:"),
+              TextFormField(
+                maxLines: 5,
+                inputFormatters: [LengthLimitingTextInputFormatter(200)],
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "You must detail the favor";
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                height: 16.0,
+              ),
+              Text("Due Date:"),
+              DateTimePickerFormField(
+                inputType: InputType.both,
+                format: DateFormat("EEEE, MMMM d, yyyy 'at' h:mma"),
+                editable: false,
+                decoration: InputDecoration(
+                    labelText: 'Date/Time', hasFloatingPlaceholder: false),
+                validator: (dateTime) {
+                  if (dateTime == null) {
+                    return "You must select a due date time for the favor";
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void save() {
+    if (_formKey.currentState.validate()) {
+      // store the favor request on firebase
+      Navigator.pop(context);
+    }
   }
 }
